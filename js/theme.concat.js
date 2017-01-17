36e5c4678c1468667698ba25da7a912474dc9795
Simon Coudeville <simon.coudeville@wijs.be>
2017-01-17 20:22:07 +0100

    first steps to animating pictures

--- js/theme.concat.js
+++ js/theme.concat.js
@@ -3,17 +3,46 @@ var chopstick =
     // init, something like a constructor
     init: function()
     {
-        chopstick.loadObject(chopstick.mobileNav, 'chopstick.mobileNav');
-        chopstick.loadObject(chopstick.hide, 'chopstick.hide');
-        chopstick.loadObject(chopstick.toggle, 'chopstick.toggle');
         chopstick.loadObject(chopstick.blazy, 'chopstick.blazy');
-        // chopstick.loadObject(chopstick.parallax, 'chopstick.parallax');
 
-        // $('.js-pictures').stellar({
-        //     horizontalScrolling: false,
-        //     // positionProperty: 'transform',
-        //     hideDistantElements: false
-        // });
+        var figures1picture1Tween = TweenMax.to('.js-scene-figures-1-picture-1', 1, {
+            css:{transform:"translateX(-50px) translateY(-50px) rotate(-4deg)"},
+            ease: Linear.easeNone
+        });
+
+        var figures1picture2Tween = TweenMax.to('.js-scene-figures-1-picture-2', 1, {
+            css:{transform:"translateX(50px) translateY(50px) rotate(3deg)"},
+            ease: Linear.easeNone
+        });
+
+        var figures1picture3Tween = TweenMax.to('.js-scene-figures-1-picture-3', 1, {
+            css:{transform:"translateX(50px) translateY(-50px) rotate(3deg)"},
+            ease: Linear.easeNone
+        });
+
+        var controller = new ScrollMagic.Controller();
+
+        var figures1picture1 = new ScrollMagic.Scene({
+            triggerElement: '.js-scene-figures-1',
+            duration: 1500
+        }).setTween(figures1picture1Tween);
+
+        var figures1picture2 = new ScrollMagic.Scene({
+            triggerElement: '.js-scene-figures-1',
+            duration: 1500
+        }).setTween(figures1picture2Tween);
+
+        var figures1picture3 = new ScrollMagic.Scene({
+            triggerElement: '.js-scene-figures-1',
+            duration: 1500
+        }).setTween(figures1picture3Tween);
+
+        controller.addScene([
+            figures1picture1,
+            figures1picture2,
+            figures1picture3
+        ]);
+
     },
 
     function(){
